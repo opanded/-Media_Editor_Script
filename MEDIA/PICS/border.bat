69931a36662552f615f3a643518edcb563ad1bdc
opanded <emaiofopanded@outlook.com>
2023-11-25 15:48:44 +0800

    new file:   MEDIA/PICS/border.bat 	renamed:    MEDIA/X2mp4.bat -> MEDIA/VEDIO/X2mp4.bat 	renamed:    MEDIA/clip.bat -> MEDIA/VEDIO/clip.bat 	renamed:    MEDIA/merge.bat -> MEDIA/VEDIO/merge.bat

---
+++ MEDIA/PICS/border.bat
@@ -0,0 +1,22 @@
+@echo off
+echo 请选择边框颜色（输入数字）：
+echo 1. 黑色
+echo 2. 白色
+echo 3. 灰色
+echo 4. 红色
+echo 5. 黄色
+echo 6. 绿色
+echo 7. 蓝色
+set /p colorChoice="请输入你的选择："
+if %colorChoice%==1 set color=black
+if %colorChoice%==2 set color=white
+if %colorChoice%==3 set color=gray
+if %colorChoice%==4 set color=red
+if %colorChoice%==5 set color=yellow
+if %colorChoice%==6 set color=green
+if %colorChoice%==7 set color=blue
+set /p borderThickness="请输入边框的厚度（像素）："
+mkdir bordered
+for %%a in (*.png *.jpg *.jpeg *.webp) do (
+    ffmpeg -i "%%a" -vf "pad=width=iw+2*%borderThickness%:height=ih+2*%borderThickness%:x=%borderThickness%:y=%borderThickness%:color=%color%" "bordered\%%~na_bordered.png"
+)
--- MEDIA/X2mp4.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-mkdir converted
-for %%a in (*.mp4 *.flv *.mkv *.avi *.mov) do (
-    ffmpeg -i "%%a" -map 0:v:0 -map 0:a:0 -codec copy "converted\%%~na.mp4"
-)
