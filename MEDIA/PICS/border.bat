b8efcdec91002337fde1ca0c19caef2da58ce66d
opanded <emaiofopanded@outlook.com>
2023-11-25 21:57:20 +0800

    modified:   MEDIA/PICS/border.bat

--- MEDIA/PICS/border.bat
+++ MEDIA/PICS/border.bat
@@ -16,6 +16,8 @@ if %colorChoice%==5 set color=yellow
 if %colorChoice%==6 set color=green
 if %colorChoice%==7 set color=blue
 set /p borderThickness="请输入边框的厚度（像素）："
+set /p directory="请输入图片所在的目录（不输入的话默认是当前目录）："
+cd /d %directory%
 mkdir bordered
 for %%a in (*.png *.jpg *.jpeg *.webp) do (
     ffmpeg -i "%%a" -vf "pad=width=iw+2*%borderThickness%:height=ih+2*%borderThickness%:x=%borderThickness%:y=%borderThickness%:color=%color%" "bordered\%%~na_bordered.png"
